e5b8ea9b66fd520f6e64c4eeb87881c6e39fb5f8
VegasGoat <UnknownGoat@gmail.com>
2015-08-15 12:56:05 -0400

    Allow anyone to turn ingots to nuggets.

--- src/main/java/com/goatsoft/magicwars/FilterCommon.java
+++ src/main/java/com/goatsoft/magicwars/FilterCommon.java
@@ -3,6 +3,7 @@ package com.goatsoft.magicwars;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
@@ -10,6 +11,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
+import tconstruct.tools.items.MaterialItem;
 
 public class FilterCommon
 {
@@ -40,17 +42,31 @@ public class FilterCommon
             // assume everything false for now
          }
 
-         // get class name of item, or underlying block for item
-         String className = result.getItem().getClass().getName();
-         if(result.getItem() instanceof ItemBlock)
+         Item itemObj = result.getItem();
+         String className = itemObj.getClass().getName();
+
+         // for block items, get the name of the block's class
+         if(itemObj instanceof ItemBlock)
          {
-            className = ((ItemBlock) result.getItem()).field_150939_a.getClass().getName();
+            className = ((ItemBlock) itemObj).field_150939_a.getClass().getName();
          }
 
          System.out.println("*** FILTER RESULT CLASS " + className);
 
-         // filter out item recipes from classes the player doesn't have
-         if(className.startsWith("thaumcraft.") && (!hasThaum))
+         boolean matched = false;
+
+         // allow anyone to turn ingots into nuggets
+         if(itemObj instanceof MaterialItem)
+         {
+            String materialName = itemObj.getUnlocalizedName(result);
+            if(materialName.endsWith("Nugget"))
+            {
+               matched = true;
+            }
+         }
+
+         // must have thaumcraft flag for thaumcraft results
+         if((!matched) && className.startsWith("thaumcraft.") && (!hasThaum))
          {
             if(!player.worldObj.isRemote)
             {
@@ -58,9 +74,12 @@ public class FilterCommon
                text.getChatStyle().setColor(EnumChatFormatting.GRAY).setItalic(true);
                player.addChatComponentMessage(text);
             }
+            matched = true;
             result = null;
          }
-         else if(className.startsWith("WayofTime.alchemicalWizardry.") && (!hasBlood))
+
+         // must have blood magic flag for blood magic results
+         if((!matched) && className.startsWith("WayofTime.alchemicalWizardry.") && (!hasBlood))
          {
             if(!player.worldObj.isRemote)
             {
@@ -68,9 +87,12 @@ public class FilterCommon
                text.getChatStyle().setColor(EnumChatFormatting.GRAY).setItalic(true);
                player.addChatComponentMessage(text);
             }
+            matched = true;
             result = null;
          }
-         else if(className.startsWith("com.emoniph.witchery.") && (!hasWitch))
+
+         // must have witchery flag for witchery results
+         if((!matched) && className.startsWith("com.emoniph.witchery.") && (!hasWitch))
          {
             if(!player.worldObj.isRemote)
             {
@@ -78,9 +100,12 @@ public class FilterCommon
                text.getChatStyle().setColor(EnumChatFormatting.GRAY).setItalic(true);
                player.addChatComponentMessage(text);
             }
+            matched = true;
             result = null;
          }
-         else if(className.startsWith("tconstruct.") && (!hasTinker))
+
+         // must have tinkers flag for tinker's construct results
+         if((!matched) && className.startsWith("tconstruct.") && (!hasTinker))
          {
             if(!player.worldObj.isRemote)
             {
